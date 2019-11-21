56b9dd79ebee8d390be27b210aafd5d3cec21c5d
Jack Quane <jackquane1@gmail.com>
2019-11-21 15:25:30 +0000

    Nav Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCardModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { ContentService } from './shared/services/content.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent
   ],
   imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ContentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
