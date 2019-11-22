94d57ea97a48de01f6f3b7015cbb99f10506e05f
Jack Quane <jackquane1@gmail.com>
2019-11-22 22:08:18 +0000

    Login functionality partially working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PageComponent } from './page/page.component'; 
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './_helpers';
 
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: PageComponent, data: {page: 'home'}},
   {path: 'about', component: PageComponent, data: {page: 'about'}},
-  {path: 'contact', component: PageComponent, data: {page: 'contact'}},
+  {path: 'contact', component: PageComponent, data: {page: 'contact'}, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginComponent},
   {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCardModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
 import { ContentService } from './shared/services/content.service';
+import { fakeBackendProvider } from './_helpers';
+import { LoginComponent } from './login/login.component';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent
+    PageComponent,
+    LoginComponent
   ],
   imports: [
     MatButtonModule,
@@ -19,9 +25,16 @@ import { ContentService } from './shared/services/content.service';
     MatToolbarModule,
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [ContentService],
+  providers: [
+    ContentService,
+    fakeBackendProvider,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
